0c11892d3ced3265048024cd95042556d69f7722
miladr0 <miladr0r@gmail.com>
2018-12-22 19:55:20 +0330

    Express app install and Running

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
+app.use(express.static('./public'));
+app.listen(3000);
+
+console.log('Express app running on port 3000');
+
+module.exports = app;
